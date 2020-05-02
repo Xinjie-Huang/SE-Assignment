b42105be831fa02c19b5f9bfb0e650a0d03f8ac1
xinjie huang <xinjie.huang.16@um.edu.mt>
2020-05-02 16:32:11 +0200

    added

---
+++ src/main/java/UoM/Test/JenkinsTest.java
@@ -0,0 +1,23 @@
+package UoM;
+
+
+public class JenkinsTest {
+    public int add(int num) {
+
+        return num + num;
+    }
+
+    public int multiply(int num) {
+        return num * num;
+    }
+
+    public int subtract(int num) {
+
+        return num - num;
+    }
+
+    public int divide(int num) {
+
+        return num / num;
+    }
+}
